c2b6358c6ea391a8c2236042ab38c2e8c2e107ac
Demonvatake <demonvatake@gmail.com>
2024-10-28 08:06:46 +0300

    fix  with my team program to work

--- notes.py
+++ notes.py
@@ -1,5 +1,6 @@
 import datetime
-
+import csv
+import os
 
 
 class Notes:
@@ -8,6 +9,7 @@ class Notes:
     def __init__(self):
         Notes.notesList.append(dict(noteid='id', txt='Заметка', tag='Тэги', Notedt="Дата и время изменения"))
 
+    @staticmethod
     def addNote(txt, tag=None):
         noteid = len(Notes.notesList)
         if tag is None:
@@ -18,16 +20,18 @@ class Notes:
 
         Notes.notesList.append(d)
 
-
+    @staticmethod
     def listNotes():
         for c in Notes.notesList:
             print(*c.values(), sep='   ')
 
+    @staticmethod
     def getDT():
         dt_now = datetime.datetime.now()
         dt_string = dt_now.strftime('%d/%m/%y %H:%M:%S')
         return dt_string
 
+    @staticmethod
     def editNotes(noteid):
         for i in range(1, len(Notes.notesList)):
             if int(Notes.notesList[i]['noteid']) == noteid:
@@ -52,7 +56,7 @@ class Notes:
             case _:
                 print("Неверная команда")
 
-
+    @staticmethod
     def delNotes(noteid):
         for i in range(1, len(Notes.notesList)):
             if int(Notes.notesList[i]['noteid']) == noteid:
@@ -60,13 +64,118 @@ class Notes:
                 print("Удалено успешно")
                 break
 
-
+    @staticmethod
     def getNotesbyDate(notesdate):
         for i in range(1, len(Notes.notesList)):
             if Notes.notesList[i]['Notedt'][:8] == notesdate:
                 print(*Notes.notesList[i].values())
 
+    @staticmethod
     def getNotesbyId(notesid):
         if len(notesid) > 0:
             for i in notesid:
                 print(*Notes.notesList[int(i)].values())
+
+
+class FileInOut:
+
+    @staticmethod
+    def toCsv(outdict, filename='notes_file.csv'):
+        if filename == '':
+            filename = 'notes_file.csv'
+        try:
+            with open(filename, 'w', newline='', encoding='utf-8') as csv_file:
+
+                fieldnames = outdict[0].keys()
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(outdict)
+                print('Файл сохранен успешно')
+        except Exception as e:
+            print('При сохранении возникла ошибка ' + str(e))
+
+    @staticmethod
+    def fromCsv(filename='notes_file.csv'):
+        if filename == '':
+            filename = 'notes_file.csv'
+        if os.path.exists(filename):
+            try:
+                with open(filename, encoding='utf-8') as csvfile:
+                    reader = csv.DictReader(csvfile)
+
+                    Notes.notesList.clear()
+                    Notes.notesList = list(reader)
+
+                    print('Данные успешно загружены')
+
+            except Exception as e:
+                print('При загрузке данных возникла ошибка ' + str(e))
+        else:
+            print(f"Файл {filename} не найден")
+
+
+def main():
+    notes = Notes()
+    files = FileInOut()
+    while True:
+        print()
+        print('Основное меню')
+        print('1 -  Добавить запись')
+        print('2 -  Удалить запись')
+        print('3 -  Редактировать запись')
+        print('4 -  Загрузить из файла')
+        print('5 -  Сохранить в файл')
+        print('6 -  Просмотреть записи')
+        print('7 -  Вывести записи за дату')
+        print('8 -  Вывести записи по id')
+        print('0 -  Выйти из программы')
+        print('Введите номер пункта меню ', end='')
+        menuItem = int(input().strip())
+
+        match menuItem:
+
+            case 0:
+                print("Выход... ")
+                break
+
+            case 1:
+                print("Введите текст заметки ", end='')
+                txt = input().strip()
+                print("Введите теги через запятую (необязательный параметр): ", end='')
+                tag = input()
+                notes.addNote(txt, tag)
+
+                notes.listNotes()
+            case 2:
+                notes.listNotes()
+                print("Введите номер заметки для удаления ", end='')
+                chid = int(input().strip())
+                Notes.delNotes(chid)
+                notes.listNotes()
+
+            case 3:
+                notes.listNotes()
+                print("Введите номер заметки для редактирования ", end='')
+                chid = int(input().strip())
+                Notes.editNotes(chid)
+
+            case 4:
+                print("Введите имя файла (ввод для имени по умолчанию 'notes_file.csv'): ", end='')
+                files.fromCsv(input().strip())
+            case 5:
+                print("Введите имя файла (ввод для имени по умолчанию 'notes_file.csv'): ", end='')
+                files.toCsv(Notes.notesList, input().strip())
+            case 6:
+                notes.listNotes()
+            case 7:
+                print("Введите дату в формате дд/мм/гг")
+                notes.getNotesbyDate(input().strip())
+            case 8:
+                print("Введите id заявок через запятую")
+                notes.getNotesbyId(input().split(','))
+            case _:
+                print("Неверная команда")
+
+
+if __name__ == '__main__':
+    main()
