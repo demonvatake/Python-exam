1bfb7389cc2344984695a469e7f1307019e85264
Demonvatake <demonvatake@gmail.com>
2024-10-28 07:56:53 +0300

    add method date and time

--- notes.py
+++ notes.py
@@ -7,3 +7,13 @@ class Notes:
 
     def __init__(self):
         Notes.notesList.append(dict(noteid='id', txt='Заметка', tag='Тэги', Notedt="Дата и время изменения"))
+
+    def addNote(txt, tag=None):
+        noteid = len(Notes.notesList)
+        if tag is None:
+            tag = []
+        else:
+            tag = list(map(lambda x: x.strip(), tag.split(',')))
+        d = dict(noteid=noteid, txt=txt, tag=tag, Notedt=Notes.getDT())
+
+        Notes.notesList.append(d)
